00804a1b10492d9a190706cbf8f0196c49a537cd
Linalii <36931084+Linalii@users.noreply.github.com>
2022-09-25 03:06:49 +0300

    Add files via upload

---
+++ Task 007/Program.cs
@@ -0,0 +1,26 @@
+﻿int [,] replaceColumnRow( int [,] array){
+    int temp;
+       for(int i = 0; i < array.GetLength(0); i++){
+            for (int j = i; j < array.GetLength(0); j++){
+                temp = array [i,j];
+                array [i,j] = array [j,i];
+                array [j,i] = temp;
+            } 
+       }     
+    return array;
+}
+void showArray (int [,] arrayToShow){
+    for(int i = 0; i < arrayToShow.GetLength(0); i++){
+        for(int j = 0; j < arrayToShow.GetLength(1); j++){
+            Console.Write(arrayToShow[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+int [,] numbers = new int [5,5]{{3,6,7,8,5},{6,8,4,7,2},{1,1,1,1,1},{5,5,5,5,5},{3,2,3,2,3}};
+showArray(numbers);
+Console.WriteLine();
+if(numbers.GetLength(0)!= numbers.GetLength(1)){
+    Console.WriteLine("Нельзя заменить столбцы на строки в неквадратной матрице!");
+}
+else showArray(replaceColumnRow(numbers));
---
+++ Task 008/Program.cs
@@ -0,0 +1,34 @@
+﻿int findMinSumRow (int [,] arrayToShow){
+    int sum = 0;
+    int temp = 0;
+    int row = 0;
+    for(int i = 0; i < arrayToShow.GetLength(0); i++){
+        for(int j = 0; j < arrayToShow.GetLength(1); j++){
+            sum = sum + arrayToShow[i,j];
+            row = i;
+        }
+        
+        if(i > 0){
+            if(temp > sum){
+                temp = sum;
+                row = i;
+            }
+        }
+        temp = sum;
+        sum = 0;
+    }
+    Console.WriteLine("Наименьшая сумма "+temp+" в строке "+(row+1));
+    return temp;
+}
+
+
+void showArray (int [,] arrayToShow){
+    for(int i = 0; i < arrayToShow.GetLength(0); i++){
+        for(int j = 0; j < arrayToShow.GetLength(1); j++){
+            Console.Write(arrayToShow[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+}
+int [,] numbers = new int [3,5]{{3,6,7,8,5},{6,8,4,7,2},{1,1,1,1,1}};
+findMinSumRow(numbers);
